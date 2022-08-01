24bdfea30bd1d3f55bfca75d1c00c9f941493ea9
iljoRSP <51939539+iljoRSP@users.noreply.github.com>
2022-08-02 01:30:55 +0800

    new service definition

--- Service.py
+++ Service.py
@@ -1,38 +1,64 @@
-
 import rpyc as r
 from rpyc.utils.server import ThreadedServer
 
+from typing import Any
+
+
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+import pandas as pd
+import numpy as np
+
+
 
 # run this first on the terminal
-# python rpyc_registry.py -l true -t 500'
+# python rpyc_registry.py -l true -t 500
 
 
+class Fold0Service(r.Service):
+    ALIASES = ['FOLD0']
 
-class MyService(r.Service):
-    ALIASES= ['MY','FOO', 'BAR']
     def on_connect(self, conn):
-        print(f"Someone connected!")
-        pass
+        print(f"Someone connected to {Fold0Service.ALIASES[0]} Service!")
+
 
     def on_disconnect(self, conn):
         # code that runs after the connection has already closed
         # (to finalize the service, if needed)
-        print(f"Someone Disconnected!")
-        pass
+        print(f"Someone disconnected from {Fold0Service.ALIASES[0]} Service!")
 
-    def exposed_get_answer(self, x): # this is an exposed method
-        return f"{x} was the input"
+        # supposedly check if something was processing and do something
 
-    exposed_the_real_answer_though = 43     # an exposed attribute
+    def exposed_ping(self) -> str:
+        return f"{Fold0Service.ALIASES[0]} Pong"
 
-    def exposed_get_question(self):  # while this method is not exposed
-        return "what is the airspeed velocity of an unladen swallow?"
+    # takes in a requestObject dictionary, performs train-test on given fold, returns replyObject
+    def exposed_train_on_fold(self, requestObject: dict[int, LinearSVC, pd.DataFrame, pd.Series, Any, Any]) -> dict():
+        # unpack object
+        id, default_model, all_data_x, all_data_y, train_index, test_index = requestObject.values()
     
+        # split all data into train and test based on given index
+        X_train, X_test = all_data_x.iloc[train_index], all_data_x.iloc[test_index]
+        y_train, y_test = all_data_y.iloc[train_index], all_data_y.iloc[test_index]
+
+        # fit the model
+        default_model.fit(X_train, y_train)
+        y_pred = default_model.predict(X_test)
+
+        # evaluate performance
+        accuracy = accuracy_score(y_test, y_pred)
+
+        # return replyObject
+        return {
+            "id": id,
+            "modified_model": default_model,  # after fitting
+            "accuracy": accuracy
+        }
 
 
 
 
 # Go to command prompt and get your ipv4 to start this server..
-t = ThreadedServer(service=MyService,hostname='192.168.103.200', port=1856, auto_register=True, listener_timeout=14)
+t = ThreadedServer(service=Fold0Service, hostname='localhost', port=1856, auto_register=True, listener_timeout=14)
 t.start()
 
